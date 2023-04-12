d5df954c2d25e9e76b43a94ebe9f494fae1bbb69
McQueenZero <82812728+McQueenZero@users.noreply.github.com>
2023-04-12 11:25:25 +0800

    V1.0.8 main.py添加InputImage, OutputImage路径存在检查，主窗口裁剪添加16:10, 21:9选项

--- BUG_RECORD.md
+++ BUG_RECORD.md
@@ -1,10 +1,16 @@
 # LINUX发布版
+打包命令：
+pyinstaller -F main.py -o ImageProcessorAPP-Vx.x.x-Linux
+dist文件夹里加入含“西工大校徽标缩小.png”的Icons文件夹
 ## V1.0.2
 1. 保存图片需要手动在图片名后面加文件扩展名（例：名字需要写成abc.png）
 2. 不能切换中文输入法，输入中文，需要从别的地方粘贴中文过来
 3. 默认保存在输入路径，默认保存文件名为 *_edit.*
 
 # WINDOWS发布版
+打包命令：
+pyinstaller -i Icons/App.ico -F main.py -w -o ImageProcessorAPP-Vx.x.x-Windows
+dist文件夹里加入含“西工大校徽标缩小.png”和“App.ico”的Icons文件夹
 ## V1.0.5
 1. 通道提取改成输出单通道图
 2. 打包时加入Icons文件夹
--- main.py
+++ main.py
@@ -5,10 +5,19 @@ import cgitb
 
 from gui import Ui_MainWindow
 
+
+def check_folder_dir(path):
+    folder_dir = os.path.join(os.getcwd(), path)
+    if not os.path.exists(folder_dir):
+        os.mkdir(folder_dir)
+
+    return folder_dir
+
+
 if __name__ == '__main__':
-    log_dir = os.path.join(os.getcwd(), 'log')
-    if not os.path.exists(log_dir):
-        os.mkdir(log_dir)
+    log_dir = check_folder_dir('log')
+    check_folder_dir('InputImages')
+    check_folder_dir('OutputImages')
     cgitb.enable(display=0, format='txt', logdir=log_dir)
 
     app = QApplication(sys.argv)
